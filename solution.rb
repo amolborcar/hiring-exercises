ce25ce8bd7d60163e7603550b05c7a8155071553
Amol Borcar <aborcar@gmail.com>
2014-10-15 15:19:01 -0700

    Basic skeleton/pseudocode for analyzing csv files.

---
+++ solution.rb
@@ -0,0 +1,25 @@
+# create method to analyze CSV files
+
+def match_records
+  # user input for filename
+  # user input for matching identifier
+
+  # read in csv file
+    # if not a csv file, throw error?
+
+  # if identifier is phone, run match_phone method
+
+  # if identifier is email, run match_email method
+
+  # output new csv
+
+end
+
+def match_phone(filename)
+  # use regex to extract 10 digit phone number, excluding special chars
+
+end
+
+def match_email(filename)
+
+end
