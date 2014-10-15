f501f337336ea1fbe64158a1fdaff599dcc86aaf
Amol Borcar <aborcar@gmail.com>
2014-10-15 15:45:26 -0700

    Basic logic to read inputs.
    Method for matching records will read in the CSV file and convert each row to a hash.  Parses input to only the keywords 'phone' and 'email' and redirects to appropriate method.  TODO: methods for matching phone and email, error if invalid CSV file entered.

--- solution.rb
+++ solution.rb
@@ -1,13 +1,35 @@
+require 'csv'
 # create method to analyze CSV files
 
 def match_records
   # user input for filename
-  # user input for matching identifier
-
+  puts "Please enter the file you want to analyze."
+  file = gets.chomp
   # read in csv file
+  inputs = []
     # if not a csv file, throw error?
+  CSV.foreach(file, :headers => true) do |row|
+    inputs << row.to_hash
+  end
+
+  p inputs
+
+  # user input for matching identifier
+  puts "Please enter the identifier you want to match by (phone or email)."
+  identifier = gets.chomp.downcase
+  while identifier != ("phone" || "email")
+    puts "Please enter either 'phone' or 'email' as an identifier."
+    identifier = gets.chomp
+  end
+
 
   # if identifier is phone, run match_phone method
+  case identifier
+    when "phone"
+      match_phone(file)
+    when "email"
+      match_email(file)
+  end
 
   # if identifier is email, run match_email method
 
@@ -17,9 +39,13 @@ end
 
 def match_phone(filename)
   # use regex to extract 10 digit phone number, excluding special chars
+  puts "You matched phone!"
 
 end
 
 def match_email(filename)
+  puts "You matched email!"
 
 end
+
+match_records
