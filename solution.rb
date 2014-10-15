3f4c2d415a6e8a3d048a252adf12233373b52035
Amol Borcar <aborcar@gmail.com>
2014-10-15 16:17:48 -0700

    Basic functionality for email matching.
    Matches email for input1, based on one email column.  TODO: implement matching based on multiple columns using same logic.  Also todo: throw error if not csv file.

--- solution.rb
+++ solution.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'debugger'
 # create method to analyze CSV files
 
 def match_records
@@ -16,19 +17,18 @@ def match_records
 
   # user input for matching identifier
   puts "Please enter the identifier you want to match by (phone or email)."
-  identifier = gets.chomp.downcase
-  while identifier != ("phone" || "email")
+  identifier = gets.chomp
+  while identifier != "phone" && identifier != "email" do
     puts "Please enter either 'phone' or 'email' as an identifier."
     identifier = gets.chomp
   end
 
-
   # if identifier is phone, run match_phone method
   case identifier
     when "phone"
-      match_phone(file)
+      match_phone(inputs)
     when "email"
-      match_email(file)
+      match_email(inputs)
   end
 
   # if identifier is email, run match_email method
@@ -37,14 +37,29 @@ def match_records
 
 end
 
-def match_phone(filename)
+def match_phone(input_rows)
   # use regex to extract 10 digit phone number, excluding special chars
   puts "You matched phone!"
 
+
 end
 
-def match_email(filename)
+def match_email(input_rows)
   puts "You matched email!"
+  id_counter = 1
+
+  emails = {}
+  input_rows.each do |row|
+    if !emails.has_key? (row['Email'])
+      emails[row['Email']] = id_counter
+      row['id'] = emails[row['Email']]
+      id_counter += 1
+    else
+      row['id'] = emails[row['Email']]
+    end
+  end
+
+  return input_rows
 
 end
 
