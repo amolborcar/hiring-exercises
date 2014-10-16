361386abc10f1a461757e52acdf8578149d1aac3
Amol Borcar <aborcar@gmail.com>
2014-10-15 17:16:32 -0700

    Created check for valid CSV file.

--- solution.rb
+++ solution.rb
@@ -4,16 +4,20 @@ require 'debugger'
 
 def match_records
   # user input for filename
-  puts "Please enter the file you want to analyze."
+  puts "Please enter the .csv file you want to analyze."
   file = gets.chomp
+
+  while file[-4..-1] != ".csv" do
+    puts "Please enter a valid .csv file, including the extension."
+    file = gets.chomp
+  end
+
   # read in csv file
   inputs = []
     # if not a csv file, throw error?
   CSV.foreach(file, :headers => true) do |row|
     inputs << row.to_hash
   end
-
-  p inputs
 
   # user input for matching identifier
   puts "Please enter the identifier you want to match by (phone or email)."
@@ -24,14 +28,13 @@ def match_records
   end
 
   # if identifier is phone, run match_phone method
+  # if identifier is email, run match_email method
   case identifier
     when "phone"
       match_phone(inputs)
     when "email"
       match_email(inputs)
   end
-
-  # if identifier is email, run match_email method
 
   # output new csv
 
